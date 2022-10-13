e14d0718f95b1b2b70808d7a1b7a35153ee51cd2
Reks <fahadadsani@gmail.com>
2022-10-13 16:29:16 +0300

    test 2

--- App.jsx
+++ App.jsx
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import Todo from './components/Todo.jsx';
-import Title from './components/Title.jsx';
-import Modal from './components/Modal.jsx';
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom"
+import Home from './pages/Home.jsx'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
     <div>
-      <Title></Title>
-      <div className='todo__wrapper'>
-        <Todo title='Finish Fes' para='code along fes s by s'></Todo>
-        <Todo title='Finish Interview' para='answer questioon'></Todo>
-        <Todo title='land hunnid kay dumpy' para='MAKE MONAYY'></Todo>
-      </div>
-      {/* <Modal title='hehe'></Modal> */}
+      <Router>
+        <Nav></Nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
